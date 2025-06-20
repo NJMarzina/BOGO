7670e3acdacab008ed4d840ba46a422c65fa893b
Nathan Marzina <46549002+NJMarzina@users.noreply.github.com>
2025-06-20 11:14:12 -0400

    coin work

--- coinflip/coinflipGame.lua
+++ coinflip/coinflipGame.lua
@@ -1,7 +1,11 @@
-local effects = require('misc.effects')  -- effects module for flashing
+local effects = require('misc.effects')
 local CoinflipGame = {}
 
-local state = "coinflip"  -- Set the initial state to coinflip
+local state = "coinflip"
+
+CoinflipGame.coinHeads = love.graphics.newImage("assets/images/coin_heads.png")
+CoinflipGame.coinTails = love.graphics.newImage("assets/images/coin_tails.png")
+CoinflipGame.currentCoin = nil
 
 local resultText = ""
 
@@ -15,14 +19,22 @@ function CoinflipGame:update(dt) end
 
 function CoinflipGame:draw()
     love.graphics.setFont(self.font)
-    love.graphics.printf(resultText, 0, love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+    love.graphics.printf(resultText, 0, love.graphics.getHeight()/2 + 80, love.graphics.getWidth(), "center")
+    if self.currentCoin then
+        love.graphics.draw(
+            self.currentCoin,
+            love.graphics.getWidth()/2 - 100,
+            love.graphics.getHeight()/2 - 100 - 40,
+            0, 200 / self.currentCoin:getWidth(), 200 / self.currentCoin:getHeight())
+    end
 end
 
 function love.keyreleased(key)
     if state == "coinflip" and key == "space" then
-        effects.flash(0.1)  -- flash effect on flip
-        local outcome = love.math.random() < 0.5 and "Heads" or "Tails"
-        resultText = "It's " .. outcome .. "!\nPress F to reset, space to insta-flip, or Q to return to launcher."
+        effects.flash(0.1)
+        local heads = love.math.random() < 0.5
+        CoinflipGame.currentCoin = heads and CoinflipGame.coinHeads or CoinflipGame.coinTails
+        resultText = "It's " .. (heads and "Heads" or "Tails") .. "!\nPress F to reset, space to insta flip, or Q to return to launcher."
     end
 end
 
